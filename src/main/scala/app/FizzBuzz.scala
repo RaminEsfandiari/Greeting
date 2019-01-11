70287e8d7990df016f02a31f3c9429d717bdee62
Ramin <ramin.esfan@gmail.com>
2019-01-11 12:27:54 +0000

    First Commit

---
+++ src/main/scala/app/FizzBuzz.scala
@@ -0,0 +1,23 @@
+package app
+
+object FizzBuzz extends App {
+
+  var x : Int = 0
+
+  while ( x < 100){
+    x = x + 1
+    if (x % 3 == 0 && x % 5 == 0){
+      println("FizzBuzz")
+    }
+    else if (x % 3 == 0){
+      println("Fizz")
+    }
+    else if (x % 5 == 0){
+      println("Buzz")
+    }
+    else {
+      println(x)
+    }
+  }
+
+}
---
+++ src/main/scala/app/GreeterApplication.scala
@@ -0,0 +1,26 @@
+package app
+
+import scala.io.StdIn
+
+object GreeterApplication extends App {
+
+  def greet(name : String) : Unit = {
+
+    if (name == "adam") {
+      println(s"You don't get a hello!")
+    }
+
+    else if(name.startsWith("a") && name.endsWith("w") ) {
+      println(s"Ey you Mr/Mrs/etc\n$name")
+    }
+
+    else {
+      println(s"Hello $name")
+    }
+  }
+
+  val name = StdIn.readLine("What is your name?\n")
+  greet(name)
+
+}
+
